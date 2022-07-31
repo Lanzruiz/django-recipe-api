f351292a10e8f7417c17edeaff36ce813e8660d0
Lanz Ruiz <Lanzruizdesigns@gmail.com>
2022-08-01 03:58:38 +0800

    Adding test case for subtractioin

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,8 @@
 def add(x, y):
     """ Add x and y and return result."""
     return x + y
+
+
+def subtract(x, y):
+    """ Substract x and y and return result."""
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -10,6 +10,14 @@ class CaclTest(SimpleTestCase):
     """ Test the calc module """
 
     def test_add_numbers(self):
+        """ Test adding the numbers """
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """ Test subtract the numbers """
+
+        res = calc.subtract(5, 2)
+
+        self.assertEqual(res, 3)
