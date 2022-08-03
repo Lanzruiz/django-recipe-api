1ca2a8be9bb7084f19eb97742f7835ff846d2f76
Lanz Ruiz <Lanzruizdesigns@gmail.com>
2022-08-03 23:03:33 +0800

    implement testing on user end point

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+from django.contrib import admin
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -26,4 +26,7 @@ urlpatterns = [
     path('api/schema', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs', SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs'),
+    path('', SpectacularSwaggerView.as_view(url_name='api-schema'),
+         name='api-docs'),
+    path('api/user', include('user.urls'))
 ]
