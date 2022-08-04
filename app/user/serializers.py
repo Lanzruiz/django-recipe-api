ff734db59ad520ecb9a3008e32b14a00e1f83e06
Lanz Ruiz <Lanzruizdesigns@gmail.com>
2022-08-04 16:03:28 +0800

    implement serializer for the auth

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -17,9 +17,9 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['email', 'password', 'name']
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
-    def create(self, validate_data):
+    def create(self, validated_data):
         """Create and return a user with excrypted password."""
-        return get_user_model().objects.create_user(**validate_data)
+        return get_user_model().objects.create_user(**validated_data)
 
 
 class AuthTokenSerializer(serializers.Serializer):
@@ -32,7 +32,7 @@ class AuthTokenSerializer(serializers.Serializer):
 
     def validate(self, attrs):
         """Validate the authenticate the user. """
-        email = attrs.get('password')
+        email = attrs.get('email')
         password = attrs.get('password')
         user = authenticate(
             request=self.context.get('request'),
@@ -40,7 +40,7 @@ class AuthTokenSerializer(serializers.Serializer):
             password=password,
         )
         if not user:
-            msg = _('Unable to authenticate with provide credentials.')
+            msg = _('Unable to authenticate with provided credentials.')
             raise serializers.ValidationError(msg, code='authentication')
 
         attrs['user'] = user
