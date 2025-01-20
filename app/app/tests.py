170fa468792cea3b4b59275a3b58f25b24301b6b
Lanz Ruiz <rolando.ruiz@metrobank.com.ph>
2025-01-20 17:22:06 +0800

    update

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,11 +6,11 @@ from django.test import SimpleTestCase
 
 from app import calc
 
-
-class CalcTest(SimpleTestCase):
+class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
     def test_add_numbers(self):
         """Test adding number together."""
+
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
